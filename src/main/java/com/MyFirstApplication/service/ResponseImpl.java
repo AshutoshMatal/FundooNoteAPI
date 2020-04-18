0efc0db35554e107d7c39e1319a1e693372777d0
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 02:29:03 +0530

    MODIFIED :Response implementation  is added

--- src/main/java/com/MyFirstApplication/service/ResponseImpl.java
+++ src/main/java/com/MyFirstApplication/service/ResponseImpl.java
@@ -1,5 +1,14 @@
 package com.MyFirstApplication.service;
 
+import com.MyFirstApplication.model.Response;
+
 public class ResponseImpl {
 
+	public static Response getResponse(String StatusMessage,int StatusCode)
+	{
+		Response responseObj=new Response();
+		responseObj.setResponseStatus(StatusMessage);
+		responseObj.setResponseCode(StatusCode);
+		return responseObj;	
+	}
 }
