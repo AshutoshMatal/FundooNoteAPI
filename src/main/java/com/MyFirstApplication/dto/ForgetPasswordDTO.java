8f956af73befb613feb2f05460dded91281772b7
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 01:39:23 +0530

    MODIFIED : Added forget password Dto with constructor and getter setters

--- src/main/java/com/MyFirstApplication/dto/ForgetPasswordDTO.java
+++ src/main/java/com/MyFirstApplication/dto/ForgetPasswordDTO.java
@@ -1,5 +1,25 @@
 package com.MyFirstApplication.dto;
 
 public class ForgetPasswordDTO {
+	long mobileNo;
+	String nickName;
+	
+	public ForgetPasswordDTO(int mobileNo, String nickName) {
+		super();
+		this.mobileNo = mobileNo;
+		this.nickName = nickName;
+	}
+	public long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
 
 }
