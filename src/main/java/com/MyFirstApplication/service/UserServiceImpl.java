ac50af33cf951cea56743d886e0a6f98cd61cba0
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 02:12:52 +0530

    MODIFIED :Only added the override methods

--- src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
+++ src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
@@ -1,5 +1,37 @@
 package com.MyFirstApplication.service;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
 
+import com.MyFirstApplication.dto.ForgetPasswordDTO;
+import com.MyFirstApplication.dto.UserLoginDTO;
+import com.MyFirstApplication.dto.UserRegisterDTO;
+import com.MyFirstApplication.model.Response;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public Response UserLogin(UserLoginDTO userLoginDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response UserRegister(UserRegisterDTO userRegisterDto) throws Exception, NullPointerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response forgetPassword(ForgetPasswordDTO forgetPasswordDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response resetPassword(int id, String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
