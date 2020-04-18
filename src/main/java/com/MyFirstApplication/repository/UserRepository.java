6e50ce91734e5aa19faeffac3c7a9122615e3e37
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 02:32:26 +0530

    MODIFIED :Jpa repository  is added with User list

--- src/main/java/com/MyFirstApplication/repository/UserRepository.java
+++ src/main/java/com/MyFirstApplication/repository/UserRepository.java
@@ -1,7 +1,24 @@
 package com.MyFirstApplication.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.MyFirstApplication.model.User;
 
-public interface UserRepository extends JpaRepository<T, ID> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	List<User> findByUserName(String userName);
 
+	List<User> findByPassword(String password);
+	
+	List<User> findByEmailId(String emailId);
+	
+	List<User> findByNickName(String nickName);
+	
+	List<User> findByMobileNo(long MobileNo);
+	
+	List<User> findByResetPassword(int id,String password);
+	
+	
 }
