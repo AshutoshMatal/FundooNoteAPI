f3a59e2c6af9eec51094e3178cba0d2e4b3dbde4
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 02:00:30 +0530

    MODIFIED : Response constructor and getter setter is added

--- src/main/java/com/MyFirstApplication/model/Response.java
+++ src/main/java/com/MyFirstApplication/model/Response.java
@@ -1,5 +1,28 @@
 package com.MyFirstApplication.model;
 
 public class Response {
+	private String ResponseStatus;
+	private int ResponseCode;
+
+	public Response() {
+		super();
+	}
+	public Response(String responseStatus, int responseCode) {
+		super();
+		ResponseStatus = responseStatus;
+		ResponseCode = responseCode;
+	}
+	public String getResponseStatus() {
+		return ResponseStatus;
+	}
+	public void setResponseStatus(String responseStatus) {
+		ResponseStatus = responseStatus;
+	}
+	public int getResponseCode() {
+		return ResponseCode;
+	}
+	public void setResponseCode(int responseCode) {
+		ResponseCode = responseCode;
+	}
 
 }
