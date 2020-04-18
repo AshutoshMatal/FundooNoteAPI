790c3cc6eac46e261d24f09121f17eb9b133baec
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 01:37:01 +0530

    MODIFIED : Proper indentation and comment is added

--- src/main/java/com/MyFirstApplication/dto/UserRegisterDTO.java
+++ src/main/java/com/MyFirstApplication/dto/UserRegisterDTO.java
@@ -1,68 +1,67 @@
 package com.MyFirstApplication.dto;
 
-	public class UserRegisterDTO 
+public class UserRegisterDTO 
+{
+	//VARIABLES
+	private String username;
+	private String name;
+	private String password;
+	private String emailId;
+	private String country;
+	//CONSTRUCTOR
+	public UserRegisterDTO(String username, String name, String password, String emailId, String country)
 	{
-		private String username;
-		private String name;
-		private String password;
-		private String emailId;
-		private String country;
-
-		public UserRegisterDTO(String username, String name, String password, String emailId, String country)
-		{
-			super();
-			this.username = username;
-			this.name = name;
-			this.password = password;
-			this.emailId = emailId;
-			this.country = country;
-		}
-
-		public String getUsername() 
-		{
-			return username;
-		}
-
-		public void setUsername(String username) 
-		{
-			this.username = username;
-		}
-
-		public String getName() 
-		{
-			return name;
-		}
-
-		public void setName(String name) 
-		{
-			this.name = name;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getEmailId() {
-			return emailId;
-		}
-
-		public void setEmailId(String emailId) {
-			this.emailId = emailId;
-		}
-
-		public String getCountry() {
-			return country;
-		}
-
-		public void setCountry(String country) {
-			this.country = country;
-		}
-
+		super();
+		this.username = username;
+		this.name = name;
+		this.password = password;
+		this.emailId = emailId;
+		this.country = country;
+	}
+	//GETTERS AND SETTERS
+	public String getUsername() 
+	{
+		return username;
 	}
 
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
 }
+
