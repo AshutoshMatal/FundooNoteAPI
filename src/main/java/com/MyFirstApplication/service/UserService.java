fb2298e602c78d7d9c64afba9b014a2a978a31cc
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 02:09:02 +0530

    MODIFIED :Added interface of user service

--- src/main/java/com/MyFirstApplication/service/UserService.java
+++ src/main/java/com/MyFirstApplication/service/UserService.java
@@ -1,5 +1,15 @@
 package com.MyFirstApplication.service;
 
-public interface UserService {
+import com.MyFirstApplication.dto.ForgetPasswordDTO;
+import com.MyFirstApplication.dto.UserLoginDTO;
+import com.MyFirstApplication.dto.UserRegisterDTO;
+import com.MyFirstApplication.model.Response;
 
+
+public interface UserService 
+{	
+	Response UserLogin(UserLoginDTO userLoginDto);
+	Response UserRegister(UserRegisterDTO userRegisterDto) throws Exception,NullPointerException;
+	Response forgetPassword(ForgetPasswordDTO forgetPasswordDto);
+	Response resetPassword(int id,String username);
 }
