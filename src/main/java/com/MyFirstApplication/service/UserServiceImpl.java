2166fb46e943b7335f85525f35e4e20061c1daa0
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 03:09:30 +0530

    MODIFIED :changes are done in the code to resolve the error

--- src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
+++ src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.MyFirstApplication.service;
 
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,44 +12,46 @@ import com.MyFirstApplication.dto.UserRegisterDTO;
 import com.MyFirstApplication.model.Response;
 import com.MyFirstApplication.model.User;
 import com.MyFirstApplication.repository.UserRepository;
-
 @Service
 public class UserServiceImpl implements UserService {
-
+	ModelMapper modelMapper=new ModelMapper();
 	@Autowired
-	UserRepository userRepository;														
+	UserRepository userRepository;
 	@Autowired
 	ResponseImpl responseImpl;
 	@Override
 	public Response UserLogin(UserLoginDTO userLoginDto) 
 	{
-		if (userRepository.findByUserName(userLoginDto.getUserName())
+	
+	if (userRepository.findByUserName(userLoginDto.getUserName())
 				.equals(userRepository.findByPassword(userLoginDto.getPassword())))
-		{
-			return new Response("Login successfull",200);
-		}
-		return new Response("already exists",400);
+	{
+		return new Response("Login successfull",200);
 	}
+	return new Response("already exists",400);
+}
 	@Override
 	public Response UserRegister(UserRegisterDTO userRegisterDto)throws Exception,NullPointerException 
 	{
-		User userRegistration=modelMapper.map( userRegisterDto,User.class);
-		userRepository.save(userRegistration);
+			User userRegistration=modelMapper.map( userRegisterDto,User.class);
+			userRepository.save(userRegistration);
 
-		return new Response("Registration successfull",200);
+			return new Response("Registration successfull",200);
 
 	}
 	@Override
 	public Response forgetPassword(ForgetPasswordDTO forgetPasswordDto)
 	{
+
 		if (userRepository.findByNickName(forgetPasswordDto.getNickName())
 				.equals(userRepository.findByMobileNo(forgetPasswordDto.getMobileNo())))
 		{
-
+				
 			return new Response("true", 200);
 		}
 		return new Response("false", 201);
 	}
+	
 
 	@Override
 	public Response resetPassword(int id, String password) 
