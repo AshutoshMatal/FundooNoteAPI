30625b7060bbfa9d188b6e3938a3a17ffcda8bc5
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-19 01:24:56 +0530

    MODIFIED : Added variables

--- src/main/java/com/MyFirstApplication/dto/UserLoginDTO.java
+++ src/main/java/com/MyFirstApplication/dto/UserLoginDTO.java
@@ -1,5 +1,31 @@
 package com.MyFirstApplication.dto;
 
 public class UserLoginDTO {
-
+	String UserName;
+	String Password;
+	public UserLoginDTO(String userName, String password)
+	{
+		super();
+		UserName = userName;
+		Password = password;
+	}
+	public String getUserName() 
+	{
+		return UserName;
+	}
+	public void setUserName(String userName)
+	{
+		UserName = userName;
+	}
+	public String getPassword() 
+	{
+		return Password;
+	}
+	public void setPassword(String password)
+	{
+		Password = password;
+	}
 }
+
+
+
