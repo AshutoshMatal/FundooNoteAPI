4d99cd7cd29d33818d5d3dd951e9e7ce20d36163
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-22 12:24:34 +0530

    MODIFIED: Changes are done in forget password to added a email service with using random  tokens

--- src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
+++ src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
@@ -1,8 +1,14 @@
 package com.MyFirstApplication.service;
 import java.util.Optional;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.MyFirstApplication.dto.ForgetPasswordDTO;
@@ -13,50 +19,66 @@ import com.MyFirstApplication.model.User;
 import com.MyFirstApplication.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService {
+
 	User user=new User();
 	ModelMapper modelMapper=new ModelMapper();
+
+	
+
+	@Autowired
+	private EmailService emailService;
+
 	@Autowired
 	UserRepository userRepository;
+	@Transactional
 	@Override
 	public Response login(UserLoginDTO userLoginDto) 
 	{
-		Optional<User> optionalUser=userRepository.findByUsername(userLoginDto.getUsername());
+		Optional<User> user=userRepository.findByUsername(userLoginDto.getUsername());
 
 		return new Response("Login successfull",200);
 
 	}
+	@Transactional
 	@Override
-	public Response register(UserRegisterDTO userRegisterDto)throws Exception
+	public Response register(UserRegisterDTO userRegisterDto)throws Exception,NullPointerException
 	{
-		modelMapper.map( userRegisterDto,user);
+		modelMapper.map(userRegisterDto,user);
 		userRepository.save(user);
-		if (userRepository.findByUsername(user.getUsername())
-				.equals(userRepository.findByPassword(user.getPassword())))
-		{
-
-			return new Response("Registration successfull",200);
-
-		}
-		return new Response("Error",400);
+		return new Response("Registration successfully",200);
 	}
+	@Transactional
 	@Override
-	public Response forget(ForgetPasswordDTO forgetPasswordDto)
+	public Response forget(String emailId)
 	{
 
-		modelMapper.map(forgetPasswordDto, user);
-
-		// Checking secret information is valid or not
-		if (userRepository.findByEmailId(user.getEmailId())
-				.equals(userRepository.findByMobileNo(user.getMobileNo())))
+		modelMapper.map(emailId,user);
+		Optional<User> optional = userRepository.findByEmailId(emailId);
+		if (!optional.isPresent())
 		{
+			return new Response("false", 201);
+		} 
+		else 
+		{
+			User user=optional.get();
+			user.setResetToken(UUID.randomUUID().toString());
+			userRepository.save(user);
+			
+			// Email message
+			SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+			passwordResetEmail.setFrom("support@demo.com");
+			passwordResetEmail.setTo(user.getEmailId());
+			passwordResetEmail.setSubject("Password Reset Request");
+			passwordResetEmail.setText("To reset your password, click the link below /reset?token=" + user.getResetToken());
+
+			emailService.sendEmail(passwordResetEmail);
 
 
 			return new Response("true", 200);
 		}
-		return new Response("false", 201);
+
 	}
-
-
+	@Transactional
 	@Override
 	public Response reset(long id, String password) 
 	{
