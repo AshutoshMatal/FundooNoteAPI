54f777a8ee4f5cb1e14366247c4435dc01e3f1f5
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-23 04:35:09 +0530

    ADDED :Confirm token repository

---
+++ src/main/java/com/MyFirstApplication/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.MyFirstApplication.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.MyFirstApplication.model.ConfirmationToken;
+
+public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
+    ConfirmationToken findByConfirmationToken(String confirmationToken);
+}
