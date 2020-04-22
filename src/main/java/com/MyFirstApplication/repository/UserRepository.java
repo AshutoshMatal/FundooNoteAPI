275fd2719ad29e44aa9a846dff15040b59737341
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-22 12:32:15 +0530

    REFACTORED:Added reset token in repository

--- src/main/java/com/MyFirstApplication/repository/UserRepository.java
+++ src/main/java/com/MyFirstApplication/repository/UserRepository.java
@@ -1,24 +1,21 @@
 package com.MyFirstApplication.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.MyFirstApplication.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-	List<User> findByUserName(String userName);
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
 
-	List<User> findByPassword(String password);
-	
-	List<User> findByEmailId(String emailId);
-	
-	List<User> findByNickName(String nickName);
-	
-	List<User> findByMobileNo(long MobileNo);
-	
-	List<User> findByResetPassword(int id,String password);
-	
-	
+	Optional<User> findByPassword(String password);
+
+	Optional<User> findByEmailId(String emailId);
+
+	Optional<User> findByMobileNo(long  mobileNo);
+
+	Optional<User> findByResetToken(String resetToken);
+
 }
