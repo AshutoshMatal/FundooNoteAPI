3fb18bec2ad6e7e703184ee17e39a473001d55db
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-24 12:17:52 +0530

    MODIFIED:Changes are done to resolve the error

--- src/main/java/com/MyFirstApplication/service/UserService.java
+++ src/main/java/com/MyFirstApplication/service/UserService.java
@@ -1,17 +1,16 @@
 package com.MyFirstApplication.service;
-
-	import com.MyFirstApplication.dto.ForgetPasswordDTO;
-	import com.MyFirstApplication.dto.UserLoginDTO;
-	import com.MyFirstApplication.dto.UserRegisterDTO;
-	import com.MyFirstApplication.model.Response;
+import com.MyFirstApplication.dto.UserLoginDTO;
+import com.MyFirstApplication.dto.UserRegisterDTO;
+import com.MyFirstApplication.model.Response;
 
 
 	public interface UserService 
 	{	
 		Response login(UserLoginDTO userLoginDto);
-		Response register(UserRegisterDTO userRegisterDto) throws Exception,NullPointerException;
-		Response forget(ForgetPasswordDTO forgetPasswordDto);
+		Response register(UserRegisterDTO userRegistrationDto)throws Exception,NullPointerException;
+		Response forget(String emailId);
 		Response reset(long id,String password);
+		String confirmToken(String confirmationToken);
 	}
 
 
