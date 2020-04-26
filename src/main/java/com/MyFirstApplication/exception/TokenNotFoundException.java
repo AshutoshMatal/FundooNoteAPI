1646b67450a56732e5d134f5d7f98ccd8015dd1d
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-26 19:20:17 +0530

    ADDED :Added token not found exception

---
+++ src/main/java/com/MyFirstApplication/exception/TokenNotFoundException.java
@@ -0,0 +1,15 @@
+package com.MyFirstApplication.exception;
+
+	public class TokenNotFoundException extends RuntimeException {
+
+		private static final long serialVersionUID = 632734427788097094L;
+
+		int errorCode;
+
+		
+		public TokenNotFoundException(int errorCode, String message) {
+
+			super(message);
+			this.errorCode = errorCode;
+		}
+}
