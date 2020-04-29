fec9bc353a6f9444172a4d000e56bb3dcef0aa51
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 11:27:47 +0530

    MODIFIED: Changes are done in this repository

--- src/main/java/com/MyFirstApplication/repository/UserRepository.java
+++ src/main/java/com/MyFirstApplication/repository/UserRepository.java
@@ -1,21 +1,12 @@
 package com.MyFirstApplication.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Repository;
 import com.MyFirstApplication.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByUsername(String username);
-
-	Optional<User> findByPassword(String password);
-
-	Optional<User> findByEmailId(String emailId);
-
-	Optional<User> findByMobileNo(long  mobileNo);
-	
-	//Optional<User> findByEmailIdIgnoreCase(String emailId);
+public interface UserRepository extends JpaRepository<User, Integer> {
+	User findByEmailId(String emailId);
 
 }
