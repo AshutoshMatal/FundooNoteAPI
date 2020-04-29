75b1d23483f4652afb0120c3fb6b88ed80e121d6
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 18:32:15 +0530

    MODIFIED:Added remaining methods and changes are done in previous code

--- src/main/java/com/MyFirstApplication/service/NoteService.java
+++ src/main/java/com/MyFirstApplication/service/NoteService.java
@@ -8,9 +8,14 @@ import com.MyFirstApplication.model.Response;
 
 
 public interface NoteService {
-Response getAllNotes();
-Response createNote(NoteDTO noteDto)throws Exception, NullPointerException;
-Response updateNote(long id,NoteDTO noteDto);
-Response  deleteNote(long id);
+Response getAllNotes(String token);
+Response createNote(String token,NoteDTO noteDto)throws Exception, NullPointerException;
+Response updateNote(String token,int id,NoteDTO noteDto);
+Response  deleteNote(String token,int id);
+Response sortByTitle(String token);
+Response sortByContent(String token);
+Response isPined(String token,int id);
+Response isTrash(String token,int id);
+Response isArchieve(String token,int id);
 
 }
