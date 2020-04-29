0cd432ab3801332a64575000c9f98b2895c35f0c
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 12:35:29 +0530

    MODIFIED:Environment variable added to all methods and changes are done to resolve the error

--- src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
+++ src/main/java/com/MyFirstApplication/service/UserServiceImpl.java
@@ -1,133 +1,165 @@
 package com.MyFirstApplication.service;
-import java.util.Optional;
 import javax.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+import com.MyFirstApplication.dto.ForgetPasswordDTO;
+import com.MyFirstApplication.dto.ResetPasswordDTO;
 import com.MyFirstApplication.dto.UserLoginDTO;
 import com.MyFirstApplication.dto.UserRegisterDTO;
-import com.MyFirstApplication.exception.EmailNotFoundException;
-import com.MyFirstApplication.exception.TokenNotFoundException;
-import com.MyFirstApplication.exception.UserException;
-import com.MyFirstApplication.model.ConfirmationToken;
-import com.MyFirstApplication.model.Note;
+import com.MyFirstApplication.exception.ForgetPasswordException;
+import com.MyFirstApplication.exception.LoginException;
+import com.MyFirstApplication.exception.RegistrationException;
+import com.MyFirstApplication.exception.ResetPasswordException;
+import com.MyFirstApplication.exception.ValidateException;
+import com.MyFirstApplication.message.MessageInfo;
+import com.MyFirstApplication.message.MessageResponse;
 import com.MyFirstApplication.model.Response;
 import com.MyFirstApplication.model.User;
-import com.MyFirstApplication.repository.ConfirmationTokenRepository;
-import com.MyFirstApplication.repository.NoteRepository;
 import com.MyFirstApplication.repository.UserRepository;
+import com.MyFirstApplication.utility.JwtToken;
+import com.sun.istack.logging.Logger;
+
 
 @Service
+@PropertySource("classpath:message.properties")
 public class UserServiceImpl implements UserService {
-	User user=new User();
-	ModelMapper modelMapper=new ModelMapper();
 
 	@Autowired
-	private ConfirmationTokenRepository confirmationTokenRepository;
+	private EmailServiceImpl emailService;
 
 	@Autowired
-	private EmailService emailService;
+	private UserRepository userRepository;
 
 	@Autowired
-	NoteRepository noteRepository;
-
+	private Environment environment;
 	@Autowired
-	UserRepository userRepository;
+	private MessageInfo message;
+	
+	@Autowired
+	private JwtToken jwtObject;
+	
+	@Autowired
+	private MessageResponse messageResponse;
+	SimpleMailMessage emailId;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	private static final Logger LOGGER = Logger.getLogger(NoteServiceImpl.class);
+
 	@Transactional
 	@Override
 	public Response login(UserLoginDTO userLoginDto) 
 	{
-		modelMapper.map(userLoginDto,user);
-		Optional<User> user=userRepository.findByUsername(userLoginDto.getUsername());
-		if(user.get().getUsername().equals(userLoginDto.getUsername())&&user.get().getPassword().equals(user))
-		{
-			return new Response("Login successfully",200);
-		}
-		throw new UserException(400,"UserName and Password doesnot matched");
-	}
+		User user = userRepository.findByEmailId(userLoginDto.getEmailId());
+		String token = jwtObject.generateToken(userLoginDto.getEmailId());
+		System.out.println(token);
+		// USER PRESENT OR NOT
+		if (user == null)
+			throw new LoginException(400,"Error");
+		// CHECK VALIDATION
+		if (user.getIsValidate()) {
+			if ((user.getPassword()).equals(userLoginDto.getPassword())) {
+				LOGGER.info("login successfully done");
+				return new Response(environment.getProperty("status.login.success"),
+						Integer.parseInt(environment.getProperty("status.success.code")), token);
+					} else {
+						return new Response(environment.getProperty("status.password.incorrect"),
+						Integer.parseInt(environment.getProperty("status.redirect.code")), message.Invalide_Password);
+					}
+				} else {
+					return new Response(environment.getProperty("status.email.notverify"),
+					Integer.parseInt(environment.getProperty("status.bad.code")), message.User_Not_Verify);
+				}
+			}
+	
+
 	@Transactional
 	@Override
 	public Response register(UserRegisterDTO userRegisterDto)throws Exception,NullPointerException
 	{
-		Optional<User> optional = userRepository.findByEmailId(user.getEmailId());
-		modelMapper.map( userRegisterDto,user);
-
-		if (userRepository.findByUsername(user.getUsername())
-				.equals(userRepository.findByPassword(user.getPassword())))
-		{
-			userRepository.save(user);
-			ConfirmationToken confirmationToken = new ConfirmationToken(user);
-
-			confirmationTokenRepository.save(confirmationToken);
-			SimpleMailMessage mailMessage = new SimpleMailMessage();
-			mailMessage.setTo(user.getEmailId());
-			mailMessage.setSubject("Complete Registration!");
-			mailMessage.setFrom("ashutoshmatal33@gmail.com");
-			mailMessage.setText("To confirm your account, please click here : "
-					+"http://localhost:8080/confirm?token="+confirmationToken.getConfirmationToken());
-			try
-			{
-				emailService.sendEmail(mailMessage);
-			} 
-			catch (EmailNotFoundException e) 
-			{
-
-				throw new EmailNotFoundException(400, "Error Email not recognised ");
-			}
-
-			return new Response("Registration successfully",200);
-
-		}
-		return new Response("Registration failed",400);
-
+		String checkEmail = userRegisterDto.getEmailId();
+		User userIsPresent = userRepository.findByEmailId(checkEmail);
+		// CHECK USER PRESENT OR NOT
+		if (userIsPresent != null) 
+			throw new RegistrationException(400,message.User_Exist);
+		User user = mapper.map(userRegisterDto, User.class);
+		String token = jwtObject.generateToken(user.getEmailId());
+		System.out.println(token);
+		emailId= messageResponse.verifyMail(user.getEmailId(), user.getUsername(), token);
+		emailService.sendEmail(emailId);
+		userRepository.save(user);
+		LOGGER.info("User Data is Successfully saved in table");
+		return new Response(environment.getProperty("status.user.register"),
+				Integer.parseInt(environment.getProperty("status.success.code")), message.Registration_Done);
 	}
 
 	@Transactional
 	@Override
-	public Response forget(String emailId)
+	public Response forget(ForgetPasswordDTO forgetPasswordDto)
 	{
 
-		modelMapper.map(emailId,user);
-		Optional<User> user = userRepository.findByEmailId(emailId);
-		if(user==null)
+		User user = userRepository.findByEmailId(forgetPasswordDto.getEmailId());
+		// CHECK USER PRESENT OR NOT
+		if (user == null)
+			throw new ForgetPasswordException(400,"Error");
+		// VALIDATION 
+		if (user.getIsValidate())
 		{
-			return new Response("false", 400);
+			String token = jwtObject.generateToken(forgetPasswordDto.getEmailId());
+			User userData = userRepository.findByEmailId(forgetPasswordDto.getEmailId());
+			System.out.println(token);
+			emailId = messageResponse.passwordReset(forgetPasswordDto.getEmailId(), user.getUsername(), token);
+			emailService.sendEmail(emailId);
+			return new Response(environment.getProperty("status.token.send"),
+					Integer.parseInt(environment.getProperty("status.success.code")), message.Token_Send);
+		} 
+		else
+		{
+			return new Response(environment.getProperty("status.email.notverify"),
+					Integer.parseInt(environment.getProperty("status.bad.code")),message.User_Not_Verify);
 		}
-		return new Response("true", 200);
 	}
 
-	@Transactional
+
 	@Override
-	public Response reset(long id, String password) 
+	public Response reset(String token,ResetPasswordDTO resetPasswordDto) 
 	{
-		Optional<User> optionalUser=userRepository.findById(id);
-		if(optionalUser.isPresent())
-		{
-			User user=optionalUser.get();
-			user.setPassword(password);
-			userRepository.save(user);
-			return new Response("Successfully done",200);
+		String checkEmail = jwtObject.getToken(token);
+		User userUpdate = userRepository.findByEmailId(checkEmail);
+		// USER PRESENT OR NOT
+		if (userUpdate == null)
+			throw new ResetPasswordException(400,"");
+		// CHECK PASSWORD AND CONFIRM ARE EQUALS OR NOT
+		if (resetPasswordDto.getConfirmPassword().equals(resetPasswordDto.getPassword())) {
+			userUpdate.setPassword(resetPasswordDto.getPassword());
+			userRepository.save(userUpdate);
+			LOGGER.info("Password is successfully update and saved in table");
+			return new Response(environment.getProperty("status.password.update"),
+			Integer.parseInt(environment.getProperty("status.success.code")), message.Update_Password);
+		} else {
+			return new Response(environment.getProperty("status.password.incorrect"),
+			Integer.parseInt(environment.getProperty("status.redirect.code")),message.Bad_Request);
 		}
-		throw new UserException(400, "Confirm password and password not matched");
 	}
 
 	@Override
-	public Response confirmToken(String confirmationToken) 
+	public Response validateUser(String token) 
 	{
-		ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
-		Optional<User> optional = userRepository.findByEmailId(token.getUser().getEmailId());
-
-		if (token!=null) 
-		{
-			User user=optional.get();
-			userRepository.save(user);
-
-			return new Response("Successfully done",200);
-
-		}
-		throw new TokenNotFoundException(400, "Token verification Failed");
+		String emailId = jwtObject.getToken(token);
+		User userIsVerified = userRepository.findByEmailId(emailId);
+		// USER PRESENT OR NOT
+		if (userIsVerified == null)
+			throw new ValidateException(400,"Error");
+		userIsVerified.setIsValidate(true);
+		userRepository.save(userIsVerified);
+		LOGGER.info("IsValidate is successfully set to true");
+		return new Response(environment.getProperty("status.email.isverify"),
+				Integer.parseInt(environment.getProperty("status.success.code")), message.Verify_User);
 	}
-
 
 }
