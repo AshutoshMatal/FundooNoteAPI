f12884b443e1222fe07eac027423f9c83b640e45
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 13:09:10 +0530

    MODIFIED:Changes are done to resolve the error

--- src/main/java/com/MyFirstApplication/controller/UserController.java
+++ src/main/java/com/MyFirstApplication/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.MyFirstApplication.controller;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,29 +14,29 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.MyFirstApplication.dto.ForgetPasswordDTO;
+import com.MyFirstApplication.dto.ResetPasswordDTO;
 import com.MyFirstApplication.dto.UserLoginDTO;
 import com.MyFirstApplication.dto.UserRegisterDTO;
+import com.MyFirstApplication.model.Note;
 import com.MyFirstApplication.model.Response;
 import com.MyFirstApplication.model.User;
+import com.MyFirstApplication.repository.ConfirmationTokenRepository;
 //import com.MyFirstApplication.repository.ConfirmationTokenRepository;
 import com.MyFirstApplication.repository.UserRepository;
 import com.MyFirstApplication.service.UserServiceImpl;
 
 @RestController
 public class UserController{
-	//@Autowired
-	//private ConfirmationTokenRepository confirmationTokenRepository;
-	@Autowired
-    UserRepository userRepository;
 	@Autowired 
 	UserServiceImpl userService;
 	
 	@PostMapping("/register")
-	public ResponseEntity<Response> register(@RequestBody UserRegisterDTO userRegisterDto)throws Exception,NullPointerException{
+	public ResponseEntity<Response> register(@Valid @RequestBody UserRegisterDTO userRegisterDto)throws Exception,NullPointerException{
 		Response response=userService.register(userRegisterDto);
 		return new ResponseEntity<Response>(response,HttpStatus.OK); 
 	}
@@ -44,20 +48,22 @@ public class UserController{
 	}
 	
 	@PostMapping("/forget")
-	public ResponseEntity<Response> forget(@RequestParam String emailId){
-		Response response=userService.forget(emailId);
+	public ResponseEntity<Response> forget( @RequestBody ForgetPasswordDTO forgetPasswordDto){
+		Response response=userService.forget(forgetPasswordDto);
 		return new ResponseEntity<Response>(response,HttpStatus.OK);
 	}
-	@PutMapping("/reset/{id}")
-	public ResponseEntity<Response> reset(@PathVariable long id,@RequestParam String password){
-		Response response=userService.reset(id,password);
+	@PutMapping("/reset")
+	public ResponseEntity<Response> reset(@RequestBody ResetPasswordDTO resetPasswordDto,@PathVariable String token){
+		Response response=userService.reset(token,resetPasswordDto);
 		return new ResponseEntity<Response>(response,HttpStatus.OK);
 	}
-	 @RequestMapping(value="/confirm", method= {RequestMethod.GET, RequestMethod.POST})
-	public ResponseEntity<Response> confirmToken( @RequestParam("token") String confirmationToken)
+	 @RequestMapping(value="/validate/{token}", method= {RequestMethod.GET, RequestMethod.POST})
+	public ResponseEntity<Response> validateUser( @RequestParam("token") String token)
 	{
-		Response response=userService.confirmToken(confirmationToken);
+		Response response=userService.validateUser(token);
 		return new ResponseEntity<Response>(response,HttpStatus.OK);
 	}
+	 
+	 
 
 }
