878518e33bee853a2767523216acac56df83111b
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 11:32:44 +0530

    MODIFIED:Added User Mapping to notes

--- src/main/java/com/MyFirstApplication/repository/NoteRepository.java
+++ src/main/java/com/MyFirstApplication/repository/NoteRepository.java
@@ -1,11 +1,20 @@
 package com.MyFirstApplication.repository;
 
-import org.springframework.data.repository.CrudRepository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.MyFirstApplication.model.Note;
+import com.MyFirstApplication.model.User;
+
 
 @Repository
-public interface NoteRepository extends CrudRepository<Note, Long> {
-
+public interface NoteRepository extends JpaRepository<Note, Integer> {
+	Note findByTitle(String title);
+	
+	Note findByContent(String content);
+	
+	Note findByUser(User user);
+	
 }
