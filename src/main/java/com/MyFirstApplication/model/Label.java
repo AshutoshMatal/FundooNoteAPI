5748cc9deee9cec6293084987f7f7cf2376ee340
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 11:08:18 +0530

    MODIFIED:Mapping is done with the user and note

--- src/main/java/com/MyFirstApplication/model/Label.java
+++ src/main/java/com/MyFirstApplication/model/Label.java
@@ -1,21 +1,39 @@
 package com.MyFirstApplication.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "label")
 public class Label {
-	private long id;
-	 private String labelName;
-	 private  long userId;
-	public Label(String labelName, long userId) {
-		super();
-		this.labelName = labelName;
-		this.userId = userId;
-	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String labelName;
+	@ManyToMany
+	@JoinTable(name = "noteLabel", joinColumns = @JoinColumn(referencedColumnName = "id"))
+	private List<Note> noteList = new ArrayList<Note>();
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
 	public Label() {
 		super();
 	}
 	public long getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getLabelName() {
@@ -24,12 +42,19 @@ public class Label {
 	public void setLabelName(String labelName) {
 		this.labelName = labelName;
 	}
-	public long getUserId() {
-		return userId;
+	public User getUser() {
+		return user;
 	}
-	public void setUserId(long userId) {
-		this.userId = userId;
+	public void setUser(User user) {
+		this.user = user;
 	}
-	 
+	public List<Note> getNoteList() {
+		return noteList;
+	}
+	public void setNoteList(List<Note> noteList) {
+		this.noteList = noteList;
+	}
+
+
 
 }
