6281e2ae9b9d3bf343abf8ff5ec19c4b5ca823ce
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-04-29 10:21:15 +0530

    ADDED :Invalid Token Exception

---
+++ src/main/java/com/MyFirstApplication/exception/InvalidTokenException.java
@@ -0,0 +1,15 @@
+package com.MyFirstApplication.exception;
+
+	public class InvalidTokenException extends RuntimeException {
+
+		private static final long serialVersionUID = 632734427788097094L;
+
+		int errorCode;
+
+		
+		public InvalidTokenException(int errorCode, String message) {
+
+			super(message);
+			this.errorCode = errorCode;
+		}
+}
